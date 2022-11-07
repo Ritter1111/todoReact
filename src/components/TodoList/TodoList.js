74cd94f8eefa7bd68b50bb6bc920209222123ce5
Ritter1111 <86166867+Ritter1111@users.noreply.github.com>
2022-11-07 17:55:59 +0200

    add some buttons

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import { React } from "react";
+import { propTypes } from "react-bootstrap/esm/Image";
 
-function TodoList() {
-    return (
-        <div>List</div>
-    );
+function TodoList({ todo, setTodo }) {
+
+  function deleteTodo(id){
+   let newTodo = [...todo].filter(item => item.id!=id);
+   setTodo(newTodo);
+  }
+  function statusTodo(id){
+    let newTodo = [...todo].filter(item => {
+      if(item.id==id){
+        item.status = !item.status;
+      }
+      return item
+  })
+    setTodo(newTodo);
+  }
+  console.log(todo)
+
+  return (
+    <div>
+      {" "}
+      {todo.map((item) => (
+        <div key={item.id}>
+        <div > {item.title} </div>
+        <button onClick={() =>deleteTodo(item.id)}>Удалить</button>
+        <button onClick={() =>statusTodo(item.id)}>Закрыть/Открыть</button>
+
+        </div>
+      ))}{" "}
+    </div>
+  );
 }
 
 export default TodoList;
+
+TodoList.propTypes = {
+    todo:propTypes.array,
+    setTodo:propTypes.function
+};
